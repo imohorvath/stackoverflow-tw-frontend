7277ad4e6368c444300125e373b5e1dd7873bda4
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-06-14 23:36:21 +0200

    implement AnswerItem component

---
+++ client/src/Components/AnswerItem/AnswerItem.jsx
@@ -0,0 +1,30 @@
+import "./AnswerItem.css";
+
+const AnswerItem = ({ answer }) => {
+
+  console.log(answer);
+
+  return (
+    <>
+      <div className="answer-box ">
+        <div className="answer-row">
+          <p className="answer-row-detail">{answer.answer_detail}</p>
+        </div>
+        <div className="answer-row">
+          <div className="answer-row-element">
+            <p>User {answer.user_id}</p>
+          </div>
+          <div className="answer-row-element">
+            <p>
+              {answer.date.split("T")[0] +
+                " " +
+                answer.date.split("T")[1].split(".")[0]}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AnswerItem;
